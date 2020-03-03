ba4b6afb39ccc742d18c13459d4d720144d52bd5
Lovemoveheartpain <885140995@qq.com>
2020-03-03 16:58:35 +0800

    修改冲突

--- Demo/src/components/Hot.vue
+++ Demo/src/components/Hot.vue
@@ -36,26 +36,16 @@ export default {
 <style scoped>
 .wai {
   background-color: white;
-<<<<<<< HEAD
   margin-top: 0.625rem;
   padding-top: 0.3125rem;
 }
 .betterDiv {
   width: 100%;
   height: 7.5rem;
-=======
-  margin-top: 10px;
-  padding-top: 5px;
-}
-.betterDiv {
-  width: 100%;
-  height: 120px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   /* background-color: pink; */
   overflow: hidden;
 }
 .hotDiv {
-<<<<<<< HEAD
   height: 7.5rem;
   display: inline-flex;
 }
@@ -63,15 +53,6 @@ export default {
   width: 6.25rem;
   height: 6.25rem;
   /* border: 0.0625rem solid red; */
-=======
-  height: 120px;
-  display: inline-flex;
-}
-.hotItem {
-  width: 100px;
-  height: 100px;
-  /* border: 1px solid red; */
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   display: inline-flex;
   flex-wrap: wrap;
   align-items: center;
@@ -83,22 +64,13 @@ export default {
 .hotItem > p {
   width: 70%;
   text-align: center;
-<<<<<<< HEAD
   font-size: 0.75rem;
-=======
-  font-size: 12px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   overflow: hidden; /*自动隐藏文字*/
   text-overflow: ellipsis; /*文字隐藏后添加省略号*/
   white-space: nowrap; /*强制不换行*/
 }
 h3 {
-<<<<<<< HEAD
   margin: 0.9375rem 0rem;
   padding-left: 0.625rem;
-=======
-  margin: 15px 0px;
-  padding-left: 10px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
 }
 </style>
--- Demo/src/components/SearchHistory.vue
+++ Demo/src/components/SearchHistory.vue
@@ -100,20 +100,12 @@ export default {
   background-color: white;
 }
 .fontcolor {
-<<<<<<< HEAD
   font-size: 0.75rem;
-=======
-  font-size: 12px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   color: rgb(47, 189, 208);
   float: right;
 }
 h4 {
-<<<<<<< HEAD
   padding: 0.9375rem;
-=======
-  padding: 15px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   background: rgb(240, 245, 246);
 }
 .searchHistoryContainer {
@@ -123,17 +115,10 @@ h4 {
   padding: 3%;
 }
 .searchHistoryContainer > div {
-<<<<<<< HEAD
   border: 0.0625rem solid gray;
   padding: 0.4375rem;
   margin: 0.3125rem;
   border-radius: 0.3125rem;
-=======
-  border: 1px solid gray;
-  padding: 7px;
-  margin: 5px;
-  border-radius: 5px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
 }
 .searchHotContainer {
   width: 90%;
@@ -142,17 +127,10 @@ h4 {
   padding: 3%;
 }
 .searchHotContainer > div {
-<<<<<<< HEAD
   border: 0.0625rem solid gray;
   padding: 0.4375rem;
   margin: 0.3125rem;
   border-radius: 0.3125rem;
-=======
-  border: 1px solid gray;
-  padding: 7px;
-  margin: 5px;
-  border-radius: 5px;
->>>>>>> 8104ffbdaa6daa3c2238ca1c85038be7c84f77ef
   color: aqua;
 }
 </style>
