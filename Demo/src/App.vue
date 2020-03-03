606a80650ece5b3e07fde5456be9831de5c42fa6
Lovemoveheartpain <885140995@qq.com>
2020-03-03 15:34:40 +0800

    提交

--- Demo/src/App.vue
+++ Demo/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-      <router-view />
+    <router-view />
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 * {
   padding: 0px;
   margin: 0px;
